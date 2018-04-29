7f46690c7ade7bf45dcff6de01a70613f1437617
Luis Güette <guetteluis@gmail.com>
2018-04-29 17:54:18 -0600

    normalization and mean centering applied to dataset

--- model.py
+++ model.py
@@ -33,14 +33,15 @@ y_train = np.array(measurements)
 
 # Build network
 from keras.models import Sequential
-from keras.layers import Flatten, Dense
+from keras.layers import Flatten, Dense, Lambda
 
 model = Sequential()
-model.add(Flatten(input_shape=(160, 320, 3)))
+model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3)))
+model.add(Flatten())
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=7)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=2)
 
 #Train model
 model.save('model.h5')
