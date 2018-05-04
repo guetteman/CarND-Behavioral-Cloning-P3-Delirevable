51beb38a1174332c7cb8bbf10ba94fadfc7386e1
Luis Güette <guetteluis@gmail.com>
2018-05-03 18:32:19 -0600

    fit_generator fixed

--- model.py
+++ model.py
@@ -114,8 +114,8 @@ model.add(Dense(10))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit_generator(train_generator, steps_per_epoch= len(train_images),
-validation_data=validation_generator, validation_steps=len(validation_images), epochs=5, verbose = 1)
+model.fit_generator(train_generator, samples_per_epoch= len(train_images),
+validation_data=validation_generator, validation_steps=len(validation_images), nb_epochs=5, verbose = 1)
 
 #Train model
 model.save('model.h5')
