f90afa45da2f0eb3213b24e145f44a33543cb5f3
Luis Güette <guetteluis@gmail.com>
2018-05-06 15:19:08 -0600

    some model changes

--- model.py
+++ model.py
@@ -20,9 +20,9 @@ def load_data_from_csv(csv_file):
 
 def angle_correction(measurement, index):
     if index == 1:
-        measurement = measurement + 0.2
+        measurement = measurement + 0.25
     elif index == 2:
-        measurement = measurement - 0.2
+        measurement = measurement - 0.25
     
     return measurement
 
@@ -149,7 +149,7 @@ validation_generator = generator(validation_images, validation_measurements, bat
 
 # Build network
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda, Cropping2D, Dropout
+from keras.layers import Flatten, Dense, Lambda, Cropping2D
 from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
 from keras.optimizers import Adam
@@ -157,24 +157,23 @@ from keras.optimizers import Adam
 model = Sequential()
 model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3)))
 model.add(Cropping2D(cropping=((70,25),(0,0))))
-model.add(Convolution2D(24, 5, 5, subsample=(2,2), activation='elu'))
-model.add(Convolution2D(36, 5, 5, subsample=(2,2), activation='elu'))
-model.add(Convolution2D(48, 5, 5, subsample=(2,2), activation='elu'))
-model.add(Convolution2D(64, 3, 3, activation='elu'))
-model.add(Convolution2D(64, 3, 3, activation='elu'))
-model.add(Dropout(0.5))
+model.add(Convolution2D(24, 5, 5, subsample=(2,2), activation='relu'))
+model.add(Convolution2D(36, 5, 5, subsample=(2,2), activation='relu'))
+model.add(Convolution2D(48, 5, 5, subsample=(2,2), activation='relu'))
+model.add(Convolution2D(64, 3, 3, activation='relu'))
+model.add(Convolution2D(64, 3, 3, activation='relu'))
 model.add(Flatten())
 model.add(Dense(100))
 model.add(Dense(50))
 model.add(Dense(10))
 model.add(Dense(1))
 
-model.compile(loss='mean_squared_error', optimizer=Adam(lr=1.0e-4))
+model.compile(loss='mse', optimizer='adam')
 # model.fit_generator(train_generator, samples_per_epoch= len(train_images), validation_data=validation_generator, validation_steps=len(validation_images), nb_epoch=5, verbose = 1)
 
 model.fit_generator(train_generator, samples_per_epoch= \
                  len(train_images), validation_data=validation_generator, \
-                 nb_val_samples=len(validation_images), nb_epoch=2, verbose=1)
+                 nb_val_samples=len(validation_images), nb_epoch=3, verbose=1)
 
 #Train model
 model.save('model.h5')
