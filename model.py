69fc7758834533f8eb4a2168b162a7b86f9b01c6
Luis Güette <guetteluis@gmail.com>
2018-05-06 11:45:25 -0600

    2 epochs

--- model.py
+++ model.py
@@ -165,7 +165,7 @@ model.compile(loss='mean_squared_error', optimizer=Adam(lr=1.0e-3))
 
 model.fit_generator(train_generator, samples_per_epoch= \
                  len(train_images), validation_data=validation_generator, \
-                 nb_val_samples=len(validation_images), nb_epoch=4, verbose=1)
+                 nb_val_samples=len(validation_images), nb_epoch=2, verbose=1)
 
 #Train model
 model.save('model.h5')
