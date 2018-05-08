4e8bf252b67a158d13e2399a370c933985c099b2
Luis Güette <guetteluis@gmail.com>
2018-05-08 00:13:28 -0600

    augmentation refactored

--- model.py
+++ model.py
@@ -60,7 +60,7 @@ def get_images(lines, base_path):
 
 def random_flip(image, measurement):
     #if np.random.rand() > 0.4:
-    if measurement > 0.2 or measurement < -0.2:
+    if measurement > 0.15 or measurement < -0.15:
         image = cv2.flip(image,1)
         measurement = measurement * -1.0
 
@@ -69,7 +69,7 @@ def random_flip(image, measurement):
         return None, None
 
 def random_translation(image, measurement, trans_range):
-    if np.random.rand() > 0.2 and (measurement > 0.2 or measurement < -0.2):
+    if np.random.rand() > 0.2 and (measurement > 0.15 or measurement < -0.15):
         rows,cols,ch = image.shape
         tr_x = trans_range*np.random.uniform()-trans_range/2
         tr_y = trans_range*np.random.uniform()-trans_range/2
