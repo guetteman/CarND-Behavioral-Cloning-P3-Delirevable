1042c545fc4289c9ce3a0c7af0c8bec7c99b38c2
Luis Güette <guetteluis@gmail.com>
2018-05-08 18:56:02 -0600

    more cleanup

--- model.py
+++ model.py
@@ -170,7 +170,6 @@ model.add(Dense(10))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-# model.fit_generator(train_generator, samples_per_epoch= len(train_images), validation_data=validation_generator, validation_steps=len(validation_images), nb_epoch=5, verbose = 1)
 
 model.fit_generator(train_generator, samples_per_epoch= \
                  len(train_images), validation_data=validation_generator, \
