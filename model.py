a9a085908ec901d90d31e24ee3544c2845d2f83d
Luis Güette <guetteluis@gmail.com>
2018-05-09 19:58:34 -0600

    not working -- normalization and lr changed

--- model.py
+++ model.py
@@ -181,7 +181,7 @@ from keras.layers.pooling import MaxPooling2D
 from keras.optimizers import Adam
 
 model = Sequential()
-model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3)))
+model.add(Lambda(lambda x: x / 127.5 - 1.0, input_shape=(160, 320, 3)))
 model.add(Cropping2D(cropping=((70,25),(0,0))))
 model.add(Convolution2D(24, 5, 5, subsample=(2,2), activation='relu'))
 model.add(Convolution2D(36, 5, 5, subsample=(2,2), activation='relu'))
@@ -194,7 +194,7 @@ model.add(Dense(50))
 model.add(Dense(10))
 model.add(Dense(1))
 
-model.compile(loss='mse', optimizer='adam', lr=1.0e-4)
+model.compile(loss='mse', optimizer='adam')
 
 model.fit_generator(train_generator, samples_per_epoch= \
                  len(train_images), validation_data=validation_generator, \
