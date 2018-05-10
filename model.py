63b4cc26c9a2752a913aa7974fa03fd7d90a0567
Luis Güette <guetteluis@gmail.com>
2018-05-09 23:56:58 -0600

    not working -- model changed again

--- model.py
+++ model.py
@@ -182,37 +182,24 @@ from keras.optimizers import Adam
 from keras.callbacks import ModelCheckpoint
 
 model = Sequential()
-# model.add(Lambda(lambda x: x / 127.5 - 1.0, input_shape=(160, 320, 3)))
-# model.add(Cropping2D(cropping=((70,25),(0,0))))
-# model.add(Convolution2D(24, 5, 5, subsample=(2,2), activation='relu'))
-# model.add(Convolution2D(36, 5, 5, subsample=(2,2), activation='relu'))
-# model.add(Convolution2D(48, 5, 5, subsample=(2,2), activation='relu'))
-# model.add(Convolution2D(64, 3, 3, activation='relu'))
-# model.add(Convolution2D(64, 3, 3, activation='relu'))
-# model.add(Flatten())
-# model.add(Dense(100))
-# model.add(Dense(50))
-# model.add(Dense(10))
-# model.add(Dense(1))
-
-model.add(Lambda(lambda x: x/127.5-1.0, input_shape=(160, 320, 3)))
-model.add(Convolution2D(24, 5, 5, activation='elu', subsample=(2, 2)))
-model.add(Convolution2D(36, 5, 5, activation='elu', subsample=(2, 2)))
-model.add(Convolution2D(48, 5, 5, activation='elu', subsample=(2, 2)))
-model.add(Convolution2D(64, 3, 3, activation='elu'))
-model.add(Convolution2D(64, 3, 3, activation='elu'))
-model.add(Dropout(0.5))
+model.add(Lambda(lambda x: x / 127.5 - 1.0, input_shape=(160, 320, 3)))
+model.add(Cropping2D(cropping=((70,25),(0,0))))
+model.add(Convolution2D(24, 5, 5, subsample=(2,2), activation='relu'))
+model.add(Convolution2D(36, 5, 5, subsample=(2,2), activation='relu'))
+model.add(Convolution2D(48, 5, 5, subsample=(2,2), activation='relu'))
+model.add(Convolution2D(64, 3, 3, activation='relu'))
+model.add(Convolution2D(64, 3, 3, activation='relu'))
 model.add(Flatten())
-model.add(Dense(100, activation='elu'))
-model.add(Dense(50, activation='elu'))
-model.add(Dense(10, activation='elu'))
+model.add(Dense(100))
+model.add(Dense(50))
+model.add(Dense(10))
 model.add(Dense(1))
 model.summary()
 
 model.compile(optimizer=Adam(lr=1e-4), loss='mse')
 
 checkpoint = ModelCheckpoint(
-    'model-{epoch:03d}.h5', 
+    './output/model-{epoch:03d}.h5', 
     monitor='val_loss', 
     verbose=0, 
     save_best_only=True, 
@@ -222,7 +209,6 @@ checkpoint = ModelCheckpoint(
 
 model.fit_generator(train_generator, samples_per_epoch= \
                  len(train_images), validation_data=validation_generator, \
-                 nb_val_samples=len(validation_images), nb_epoch=20, callbacks=[checkpoint], verbose=1)
+                 nb_val_samples=len(validation_images), nb_epoch=6, callbacks=[checkpoint], verbose=1)
 
-#Train model
-model.save('model.h5')
+#model.save('model.h5')
