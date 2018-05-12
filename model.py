ba5a00026cb9cbf26fec09b87a2095c17afb27ed
Luis Güette <guetteluis@gmail.com>
2018-05-12 10:59:45 -0600

    testing with new code

--- model.py
+++ model.py
@@ -33,7 +33,7 @@ def get_images(lines, base_path):
 
     images = []
     measurements = []
-
+        
     for line in lines:
         for i in range(3):
 
@@ -41,19 +41,19 @@ def get_images(lines, base_path):
             filename = source_path.split('/')[-1]
             current_path = base_path + filename
 
-            if (np.random.rand() > 0.2 and float(line[3]) < 0.005 and float(line[3]) > -0.005) or (float(line[3]) >= 0.005 or float(line[3]) <= -0.005):
+            image = cv2.imread(current_path)
+            images.append(image)
 
-                image = cv2.imread(current_path)
-                images.append(image)
-
-                # angle corrections
-                measurement = angle_correction(float(line[3]), i)
-                measurements.append(round(measurement, 2))
+            # angle corrections
+            measurement = angle_correction(float(line[3]), i)
+            measurements.append(round(measurement, 2))
     
-    return images,measurements
+    images_measurements = list(zip(images, measurements))
+    random.shuffle(images_measurements)
+
+    return zip(*images_measurements)
 
 def plot_distribution_chart(x, y, xlabel, ylabel, width, color, location):
-  
     plt.figure(figsize=(15,7))
     plt.ylabel(ylabel, fontsize=18)
     plt.xlabel(xlabel, fontsize=16)
@@ -61,7 +61,7 @@ def plot_distribution_chart(x, y, xlabel, ylabel, width, color, location):
     plt.savefig(location)
 
 def random_flip(image, measurement):
-    if measurement > 0.005 or measurement < -0.005:
+    if np.random.rand() > 0.5:
         image = cv2.flip(image,1)
         measurement = measurement * -1.0
 
@@ -70,41 +70,33 @@ def random_flip(image, measurement):
         return None, None
 
 def random_translation(image, measurement, trans_range):
-    if np.random.rand() > 0.2:
-        rows,cols,ch = image.shape
-        tr_x = trans_range*np.random.uniform()-trans_range/2
-        tr_y = trans_range*np.random.uniform()-trans_range/2
-        Trans_M = np.float32([[1,0,tr_x],[0,1,tr_y]])
+    rows,cols,ch = image.shape
+    tr_x = trans_range*np.random.uniform()-trans_range/2
+    tr_y = trans_range*np.random.uniform()-trans_range/2
+    Trans_M = np.float32([[1,0,tr_x],[0,1,tr_y]])
 
-        image = cv2.warpAffine(image,Trans_M,(cols,rows))
-        measurement += round(tr_x * 0.001, 2)
+    image = cv2.warpAffine(image,Trans_M,(cols,rows))
+    measurement += round(tr_x * 0.002, 2)
 
-        return image, measurement
+    return image, measurement
 
-    else: 
-        return None, None
-
-def random_brightness(image, measurement):
-    if (measurement > 0.05 or measurement < -0.05):
-
-        hsv = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
+def random_brightness(image):
+    hsv = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
         
-        random_value = np.random.rand()
+    random_value = np.random.rand()
 
-        if random_value > 0.5:
-            ratio = 1 + random_value - 0.5
-        else:
-            ratio = random_value
-        
-        hsv[:,:,2] =  hsv[:,:,2] * ratio
-        image = cv2.cvtColor(hsv, cv2.COLOR_HSV2RGB)
-    
-        return image
+    if random_value > 0.5:
+        ratio = 1 + random_value - 0.5
     else:
-        return None
+        ratio = random_value
+    
+    hsv[:,:,2] =  hsv[:,:,2] * ratio
+    image = cv2.cvtColor(hsv, cv2.COLOR_HSV2RGB)
+
+    return image
 
 def add_to_augmented_data(augmented_image, augmented_measurement, augmented_images, augmented_measurements):
-    if augmented_image is not None:
+    if augmented_image is not None and augmented_measurement is not None:
             augmented_images.append(augmented_image)
             augmented_measurements.append(augmented_measurement)
     
@@ -119,66 +111,57 @@ def augment_data(images, measurements, _classes, counts):
         augmented_images.append(image)
         augmented_measurements.append(measurement)
         
+        max_counts = np.amax(counts)
         i, = np.where(_classes == measurement)
-        if counts[i] < 300:
-            augmented_image, augmented_measurement = random_flip(image, measurement)
-            augmented_images, augmented_measurement = add_to_augmented_data(augmented_image, augmented_measurement, augmented_images, augmented_measurements)        
 
-            augmented_image, augmented_measurement = random_translation(image, measurement, 5)
-            augmented_images, augmented_measurement = add_to_augmented_data(augmented_image, augmented_measurement, augmented_images, augmented_measurements)
+        if counts[i]/max_counts < 0.1:
+            for i in range(5):
+                augmented_image, augmented_measurement = random_flip(image, measurement)
+                augmented_image, augmented_measurement = random_translation(augmented_image, augmented_measurement, 5)
+                augmented_image = random_brightness(augmented_image, augmented_measurement)
+                
+                add_to_augmented_data(augmented_image, augmented_measurement, augmented_images, augmented_measurements)
 
-            augmented_image = random_brightness(image, measurement)
-            augmented_images, augmented_measurement = add_to_augmented_data(augmented_image, measurement, augmented_images, augmented_measurements)
-
-        if counts[i] < 100:
-            for i in range(4):
-
-                augmented_image, augmented_measurement = random_translation(image, measurement, 5)
-                augmented_images, augmented_measurement = add_to_augmented_data(augmented_image, augmented_measurement, augmented_images, augmented_measurements)
-
-                augmented_image = random_brightness(image, measurement)
-                augmented_images, augmented_measurement = add_to_augmented_data(augmented_image, measurement, augmented_images, augmented_measurements)
+        if counts[i] < 0.05:
+            for i in range(10):
+                augmented_image, augmented_measurement = random_flip(image, measurement)
+                augmented_image, augmented_measurement = random_translation(augmented_image, augmented_measurement, 5)
+                augmented_image = random_brightness(augmented_image)
+                
+                add_to_augmented_data(augmented_image, augmented_measurement, augmented_images, augmented_measurements)
 
     return augmented_images,augmented_measurements
 
-def generator(images, measurements, batch_size=32):
+def generator(lines, batch_size=32, training=False):
     
     num_samples = len(images)
     
     while 1:
-        for offset in range(0, num_samples, batch_size):
-            batch_images = images[offset:offset+batch_size]
-            batch_measurements = measurements[offset:offset+batch_size]
 
-            # trim image to only see section with road
+        for offset in range(0, len(lines), batch_size):
+            batch_lines = lines[offset:offset+batch_size]
+
+            batch_images, batch_measurements = get_images(lines, 'data/IMG/')
+
+            if training:
+                _classes, counts = np.unique(np.array(batch_measurements), return_counts=True)
+                batch_images, batch_measurements = augment_data(batch_images, batch_measurements, _classes, counts)
+
             X_train = np.array(batch_images)
             y_train = np.array(batch_measurements)
+
             yield sklearn.utils.shuffle(X_train, y_train)
+
+
 
 #Main code
 
 # Load images paths from csv file
 lines = load_data_from_csv('data/driving_log.csv')
 
-# Fix images path to use in another machine like AWS
-images, measurements = get_images(lines, 'data/IMG/')
-
-# Plotting Raw Data
-_classes, counts = np.unique(np.array(measurements), return_counts=True)
-plot_distribution_chart(_classes, counts, 'Classes', '# Training Examples', 0.002, 'blue', './images/dataset-distribution.png')
-
-# Data augmentation
-augmented_images, augmented_measurements = augment_data(images, measurements, _classes, counts)
-
-# Plotting augmented data
-_classes, counts = np.unique(np.array(augmented_measurements), return_counts=True)
-plot_distribution_chart(_classes, counts, 'Classes', '# Training Examples', 0.002, 'blue', './images/augmented-dataset-distribution.png')
-
-train_images, validation_images, train_measurements, validation_measurements = train_test_split(augmented_images, augmented_measurements, test_size=0.20)
-
 # compile and train the model using the generator function
-train_generator = generator(train_images, train_measurements, batch_size=32)
-validation_generator = generator(validation_images, validation_measurements, batch_size=32)
+train_generator = generator(lines, batch_size=1000, training=True)
+train_generator = generator(lines, batch_size=1000)
 
 # Build network
 from keras.models import Sequential
@@ -209,7 +192,7 @@ checkpoint = ModelCheckpoint(
     'model-{epoch:03d}.h5', 
     monitor='val_loss', 
     verbose=0, 
-    save_best_only=True, 
+    save_best_only=False, 
     save_weights_only=False, 
     mode='auto', 
     period=1)
@@ -217,5 +200,3 @@ checkpoint = ModelCheckpoint(
 model.fit_generator(train_generator, samples_per_epoch= \
                  len(train_images), validation_data=validation_generator, \
                  nb_val_samples=len(validation_images), nb_epoch=10, callbacks=[checkpoint], verbose=1)
-
-#model.save('model.h5')
